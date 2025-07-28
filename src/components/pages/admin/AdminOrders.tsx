1d0ad1ca1ac36143c2bad6e949e3e6e7b7f13087
manh <manh99294@donga.edu.vn>
2025-07-28 09:57:13 +0700

    a

--- src/components/pages/admin/AdminOrders.tsx
+++ src/components/pages/admin/AdminOrders.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FiSearch, FiChevronDown, FiEye, FiCheck, FiArrowLeft, FiFileText } from 'react-icons/fi';
-import '../../../assets/css/Dashboard.css';
+// import '../../../assets/css/Dashboard.css';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
